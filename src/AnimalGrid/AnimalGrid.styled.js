cb56e90231d8545e9ecfd35bfeab47e298d47641
RuntimeTerror10 <parthb.git@gmail.com>
2022-02-04 01:57:37 +0530

    revert changes on ggrid

--- src/AnimalGrid/AnimalGrid.styled.js
+++ src/AnimalGrid/AnimalGrid.styled.js
@@ -3,8 +3,6 @@ import styled from "styled-components";
 export const StyledGrid = styled.div`
   display: flex;
   flex-wrap: wrap;
-  width: 75%;
-  margin: 0 10px;
   justify-content: center;
   animation: zoomIn 0.55s ease-in-out;
 
@@ -17,5 +15,26 @@ export const StyledGrid = styled.div`
       opacity: 1;
       transform: scale(1);
     }
+  }
+
+  @media (min-width: 300px) {
+    width: 95%;
+  }
+
+  @media (min-width: 660px) {
+    width: 90%;
+    margin-top: 0;
+  }
+  @media (min-width: 1000px) {
+    width: 80%;
+    margin-top: 0;
+  }
+  @media (min-width: 1600px) {
+    width: 70%;
+    margin-top: 0;
+  }
+  @media (min-width: 1900px) {
+    width: 60%;
+    margin-top: 0;
   }
 `;
